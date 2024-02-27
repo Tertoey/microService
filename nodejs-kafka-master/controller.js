919e6d9f57664adfcfb410911efde7264bdf74a6
Tertoey <tanawat.toey@gmail.com>
2024-02-27 23:17:08 +0700

    R1

---
+++ nodejs-kafka-master/controller.js
@@ -0,0 +1,37 @@
+import KafkaConfig from "./config.js";
+
+const sendMessageToKafka = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const kafkaConfig = new KafkaConfig();
+    const messages = [{ key: "key1", value: JSON.stringify(message) }];
+    kafkaConfig.produce("my-topic", messages);
+
+    res.status(200).json({
+      status: "Ok!",
+      message: "Message successfully send!",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// const sendMessageToKafka = async (req, res) => {
+//   try {
+//     const { message } = req.body;
+//     const kafkaConfig = new KafkaConfig();
+//     const messages = [{ key: "key1", value: req.body }];
+//     kafkaConfig.produce("my-topic", JSON.stringify(messages));
+
+//     res.status(200).json({
+//       status: "Ok!",
+//       message: "Message successfully send!",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+const constrollers = { sendMessageToKafka };
+
+export default constrollers;
